6e527f72bab48ed7ff0475846788f4830a035c95
Liangrui Lu <liangrui@cs.ualberta.ca>
2015-01-28 16:24:21 -0700

    test

---
+++ lonelyTwitter/lonelyTwitterTests/src/ca/ualbeta/cs/lonelywitter/test/LonelyTwitterModelTest.java
@@ -0,0 +1,22 @@
+package ca.ualbeta.cs.lonelywitter.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class LonelyTwitterModelTest extends
+		ActivityInstrumentationTestCase2<LonelyTwitterActivity>
+{
+
+	public LonelyTwitterModelTest(String name)
+	{
+
+		super(name);
+	}
+
+	protected void setUp() throws Exception
+	{
+
+		super.setUp();
+	}
+
+}
